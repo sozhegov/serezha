0197403f63918707d4fe46a3443ba3c169c6e086
Ozhegov Alexander <ozhegov.a@i20.team>
2025-07-30 10:13:13 +0800

    Update translate it game

--- translate_it/translate_it.js
+++ translate_it/translate_it.js
@@ -7,12 +7,58 @@ class TranslateIt {
         this.fails = 0;
         this.currentWord = "";
         this.gameContainer = gameContainer;
-        // TODO: строим верстку контейнера с игрой
+        this.initGameContainer();
         this.pointsElement = gameContainer.querySelector("#points");
         this.phraseElement = gameContainer.querySelector("#phrase");
         this.translationElement = gameContainer.querySelector("#translation");
         this.dictionary = dictionary;
+        this.inversedDictionary = this.inverseDictionary(dictionary);
+        this.mode = 0;
         this.variantSelected = false;
+        this.currentDictionary = {};
+    }
+
+    initGameContainer() {
+        this.gameContainer.innerHTML = "";
+
+        let h3 = document.createElement("h3");
+        h3.innerHTML = "Переведи фразу";
+        this.gameContainer.appendChild(h3);
+
+        const selectDiv = document.createElement("div");
+        selectDiv.classList.add("select-variant");
+
+        const phraseDiv = document.createElement("div");
+        phraseDiv.innerHTML = "";
+        phraseDiv.setAttribute("id", "phrase");
+        selectDiv.appendChild(phraseDiv);
+
+        h3 = document.createElement("h3");
+        h3.innerHTML = "Выбери вариант ответа";
+        selectDiv.appendChild(h3);
+
+        const translationDiv = document.createElement("div");
+        translationDiv.setAttribute("id", "translation");
+        selectDiv.appendChild(translationDiv);
+
+        this.gameContainer.appendChild(selectDiv);
+
+        const h2 = document.createElement("h2");
+        h2.innerHTML = "Счет";
+        this.gameContainer.appendChild(h2);
+
+        const pointsDiv = document.createElement("div");
+        pointsDiv.innerHTML = "0";
+        pointsDiv.setAttribute("id", "points");
+        this.gameContainer.appendChild(pointsDiv);
+    }
+
+    inverseDictionary(dictionary) {
+        const result = {};
+        Object.entries(dictionary).forEach(([key, value]) => {
+            result[value] = key;
+        });
+        return result;
     }
 
     startGame(event) {
@@ -36,15 +82,21 @@ class TranslateIt {
     }
 
     askWord() {
-        const words = Object.keys(this.dictionary);
-        const translations = Object.values(this.dictionary);
+        this.setRandomMode();
+        if (this.mode === 0) {
+            this.currentDictionary = JSON.parse(JSON.stringify(this.dictionary));
+        } else {
+            this.currentDictionary = JSON.parse(JSON.stringify(this.inversedDictionary));
+        }
+        const words = Object.keys(this.currentDictionary);
+        const translations = Object.values(this.currentDictionary);
         const wordIndex = this.getRandomIndex(words.length);
         this.currentWord = words[wordIndex];
         this.phraseElement.innerHTML = this.currentWord;
-        const translateVariants = [this.dictionary[this.currentWord]];
+        const translateVariants = [this.currentDictionary[this.currentWord]];
         while (translateVariants.length < 4) {
             const variantIndex = this.getRandomIndex(words.length);
-            if (variantIndex !== wordIndex) {
+            if (variantIndex !== wordIndex && !translateVariants.includes(translations[variantIndex])) {
                 translateVariants.push(translations[variantIndex]);
             }
         }
@@ -63,6 +115,10 @@ class TranslateIt {
         this.variantSelected = false;
     }
 
+    setRandomMode() {
+        this.mode = Math.round(Math.random());
+    }
+
     increasePoints() {
         this.points += this.pointsStep;
         this.pointsElement.innerHTML = this.points;
@@ -77,7 +133,7 @@ class TranslateIt {
             this.variantSelected = true;
             const translateVariants = document.querySelectorAll(".translate-variant");
             const selectedVariant = event.target.innerHTML;
-            if (selectedVariant === game.dictionary[game.currentWord]) {
+            if (selectedVariant === game.currentDictionary[game.currentWord]) {
                 game.increasePoints();
                 game.flashGreen(event.target);
                 setTimeout(function () {
@@ -90,7 +146,7 @@ class TranslateIt {
                 } else {
                     game.flashRed(event.target);
                     translateVariants.forEach((translateVariant) => {
-                        if (translateVariant.innerHTML === game.dictionary[game.currentWord]) {
+                        if (translateVariant.innerHTML === game.currentDictionary[game.currentWord]) {
                             game.flashGreen(translateVariant);
                         }
                     });
